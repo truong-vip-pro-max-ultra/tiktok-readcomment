b0eab669b8d737d3e1d83ccef3aee29fef314d9d
truong-vip-pro-max-ultra <truongvippromaxultra@gmail.com>
2025-05-17 23:03:30 +0700

    add speed mp3

--- utils.py
+++ utils.py
@@ -6,11 +6,12 @@ def translate_text(text):
     return GoogleTranslator(source='auto', target='vi').translate(text)
 
 def change_speed(input_file, output_file, speed_factor):
-    audio, sample_rate = sf.read(input_file)
-
-    adjusted_audio = signal.resample(audio, int(len(audio) / speed_factor))
-
-    sf.write(output_file, adjusted_audio, sample_rate)
+    # audio, sample_rate = sf.read(input_file)
+    #
+    # adjusted_audio = signal.resample(audio, int(len(audio) / speed_factor))
+    #
+    # sf.write(output_file, adjusted_audio, sample_rate)
+    pass
 
 def convert_username_youtube(username):
     username = re.sub(r'\W+', '', username)
